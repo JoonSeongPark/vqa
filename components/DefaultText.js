ab7fd4266be8d0a8faa58dd8a5672c02745853a8
JoonSeongPark <rytt@yonsei.ac.kr>
2019-12-25 15:42:57 +0900

    default text component update

---
+++ components/DefaultText.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const DefaultText = props => {
+  return <Text style={{...styles.text,...props.style}} >{props.children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "kakao"
+  }
+});
+
+export default DefaultText;
