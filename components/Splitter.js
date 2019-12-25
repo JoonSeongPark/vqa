e76bf412a8d22b1a4a24fc168bc5991fd05d4bc2
JoonSeongPark <rytt@yonsei.ac.kr>
2019-12-25 20:07:06 +0900

    split line component update

---
+++ components/Splitter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+const Splitter = props => {
+  return <View style={styles.line}></View>
+}
+
+const styles = StyleSheet.create({
+line: {
+  borderColor: '#ccc',
+  borderBottomWidth: 0.5,
+  marginVertical: 7
+}
+})
+
+export default Splitter
