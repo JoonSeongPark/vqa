1103057fd7fea54fabc888405e9c7c1da62d5093
JoonSeongPark <rytt@yonsei.ac.kr>
2019-12-26 19:33:56 +0900

    VQA Result component update

--- components/AnswerList.js
+++ components/AnswerList.js
@@ -1,100 +1,185 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  PixelRatio,
+  Dimensions,
+  Platform
+} from "react-native";
 import DefaultText from "./DefaultText";
 import Colors from "../constants/Colors";
+
+var defaultTitleFontSize = 24;
+var defaultRadiusSize = 10;
+
+if (PixelRatio.get() === 3) {
+  defaultTitleFontSize = 21;
+  defaultRadiusSize = 9;
+} else if (PixelRatio.get() === 2) {
+  defaultTitleFontSize = 18;
+  defaultRadiusSize = 8;
+} else if (PixelRatio.get() < 2) {
+  defaultTitleFontSize = 15;
+  defaultRadiusSize = 7;
+}
 
 const AnswerList = props => {
   return (
     <View>
       <View style={styles.resultLine}>
-        <View style={styles.answer}>
-          <DefaultText>{props.result[0].answer}</DefaultText>
-        </View>
-        <View style={styles.probContainer}>
-          <View style={{...styles.prob,...{width: props.result[0].prob+'%'}}}>
-          </View>
-            <DefaultText >
-              {props.result[0].prob.toFixed(2)+' %'}
+        <DefaultText style={styles.answer}>
+          {props.result[0].answer}
+        </DefaultText>
+        <View style={styles.barContainer}>
+          <View
+            style={{
+              ...styles.barView,
+              ...{
+                width: props.result[0].prob + "%",
+                borderTopRightRadius:
+                  props.result[0].prob == 100 ? 5 : defaultRadiusSize,
+                borderBottomRightRadius:
+                  props.result[0].prob == 100 ? 5 : defaultRadiusSize
+              }
+            }}
+          />
+          <View style={styles.textPosition}>
+            <DefaultText style={styles.probText}>
+              {props.result[0].prob.toFixed(2) + " %"}
             </DefaultText>
+          </View>
         </View>
       </View>
       <View style={styles.resultLine}>
-        <View style={styles.answer}>
-          <DefaultText>{props.result[1].answer}</DefaultText>
-        </View>
-        <View style={styles.probContainer}>
-          <View style={{...styles.prob,...{width: props.result[1].prob+'%'}}}>
-          </View>
-            <DefaultText >
-              {props.result[1].prob.toFixed(2)+' %'}
+        <DefaultText style={styles.answer}>
+          {props.result[1].answer}
+        </DefaultText>
+        <View style={styles.barContainer}>
+          <View
+            style={{
+              ...styles.barView,
+              ...{
+                width: props.result[1].prob + "%",
+                borderTopRightRadius:
+                  props.result[1].prob == 100 ? 5 : defaultRadiusSize,
+                borderBottomRightRadius:
+                  props.result[1].prob == 100 ? 5 : defaultRadiusSize
+              }
+            }}
+          />
+          <View style={styles.textPosition}>
+            <DefaultText style={styles.probText}>
+              {props.result[1].prob.toFixed(2) + " %"}
             </DefaultText>
+          </View>
         </View>
       </View>
       <View style={styles.resultLine}>
-        <View style={styles.answer}>
-          <DefaultText>{props.result[2].answer}</DefaultText>
-        </View>
-        <View style={styles.probContainer}>
-          <View style={{...styles.prob,...{width: props.result[2].prob+'%'}}}>
-          </View>
-            <DefaultText >
-              {props.result[2].prob.toFixed(2)+' %'}
+        <DefaultText style={styles.answer}>
+          {props.result[2].answer}
+        </DefaultText>
+        <View style={styles.barContainer}>
+          <View
+            style={{
+              ...styles.barView,
+              ...{
+                width: props.result[2].prob + "%",
+                borderTopRightRadius:
+                  props.result[2].prob == 100 ? 5 : defaultRadiusSize,
+                borderBottomRightRadius:
+                  props.result[2].prob == 100 ? 5 : defaultRadiusSize
+              }
+            }}
+          />
+          <View style={styles.textPosition}>
+            <DefaultText style={styles.probText}>
+              {props.result[2].prob.toFixed(2) + " %"}
             </DefaultText>
+          </View>
         </View>
       </View>
       <View style={styles.resultLine}>
-        <View style={styles.answer}>
-          <DefaultText>{props.result[3].answer}</DefaultText>
-        </View>
-        <View style={styles.probContainer}>
-          <View style={{...styles.prob,...{width: props.result[3].prob+'%'}}}>
-          </View>
-            <DefaultText >
-              {props.result[3].prob.toFixed(2)+' %'}
+        <DefaultText style={styles.answer}>
+          {props.result[3].answer}
+        </DefaultText>
+        <View style={styles.barContainer}>
+          <View
+            style={{
+              ...styles.barView,
+              ...{
+                width: props.result[3].prob + "%",
+                borderTopRightRadius:
+                  props.result[3].prob == 100 ? 5 : defaultRadiusSize,
+                borderBottomRightRadius:
+                  props.result[3].prob == 100 ? 5 : defaultRadiusSize
+              }
+            }}
+          />
+          <View style={styles.textPosition}>
+            <DefaultText style={styles.probText}>
+              {props.result[3].prob.toFixed(2) + " %"}
             </DefaultText>
+          </View>
         </View>
       </View>
       <View style={styles.resultLine}>
-        <View style={styles.answer}>
-          <DefaultText>{props.result[4].answer}</DefaultText>
-        </View>
-        <View style={styles.probContainer}>
-          <View style={{...styles.prob,...{width: props.result[4].prob+'%'}}}>
-          </View>
-            <DefaultText >
-              {props.result[4].prob.toFixed(2)+' %'}
+        <DefaultText style={styles.answer}>
+          {props.result[4].answer}
+        </DefaultText>
+        <View style={styles.barContainer}>
+          <View
+            style={{
+              ...styles.barView,
+              ...{
+                width: props.result[4].prob + "%",
+                borderTopRightRadius:
+                  props.result[4].prob == 140 ? 5 : defaultRadiusSize,
+                borderBottomRightRadius:
+                  props.result[4].prob == 100 ? 5 : defaultRadiusSize
+              }
+            }}
+          />
+          <View style={styles.textPosition}>
+            <DefaultText style={styles.probText}>
+              {props.result[4].prob.toFixed(2) + " %"}
             </DefaultText>
+          </View>
         </View>
       </View>
-      
-      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   resultLine: {
-    flexDirection: "row",
     paddingVertical: 5
   },
   answer: {
-    paddingHorizontal: 5,
-    width: "30%",
+    fontSize: defaultTitleFontSize,
+    paddingHorizontal: Dimensions.get("window").width * 0.015,
+    paddingVertical: Dimensions.get("window").height * 0.005
+  },
+  barContainer: {
+    width: "100%",
+    height: Dimensions.get("window").height * 0.03,
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+    overflow: "hidden"
+  },
+  barView: {
+    backgroundColor: Colors.blueColor,
+    height: "100%"
+  },
+  textPosition: {
+    position: "absolute",
     alignItems: "center",
-    justifyContent: 'center'
+    justifyContent: "center",
+    right: 0,
+    left: 0,
+    top: 0,
+    bottom: 0
   },
-  probContainer: {
-    borderLeftWidth:1,
-    height: 40,
-    width: "65%",
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  prob: {
-    height: '70%',
-    marginRight: 5,
-    backgroundColor: Colors.mainColor
-  }
+  probText: {}
 });
 
 export default AnswerList;
