96c607e03a420ee326890e81bfcbf35e03816b72
JoonSeongPark <rytt@yonsei.ac.kr>
2019-12-28 01:58:52 +0900

    DefaultText modified

--- components/DefaultText.js
+++ components/DefaultText.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
 
 const DefaultText = props => {
-  return <Text style={{...styles.text,...props.style}} >{props.children}</Text>;
+  return <Text style={{...styles.text,...props.style}} onPress={props.onPress} >{props.children}</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontFamily: "kakao"
+    fontFamily: "kakao",
+    color: Colors.mainColor
   }
 });
 
